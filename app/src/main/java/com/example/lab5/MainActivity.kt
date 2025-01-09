8eb4e23c68d7514b1e2c7af25c4a23b8d3f40bb3
Ufgt_NOI <strelok1579@gmail.com>
2025-01-09 10:18:27 +0700

    логика основной активити

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,10 +1,17 @@
-package com.example.lab5
+package com.example.laba5
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.lab5.MainActivity2
+import com.example.lab5.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,32 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val etHours = findViewById<EditText>(R.id.etHours)
+        val radioGroup = findViewById<RadioGroup>(R.id.radioGroup)
+        val btnOk = findViewById<Button>(R.id.btnOk)
+
+        btnOk.setOnClickListener {
+            val hoursStr = etHours.text.toString()
+            if (hoursStr.isEmpty()) {
+                Toast.makeText(this, "Введите количество часов", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            val hours = hoursStr.toInt()
+            var costPerHour = 2000
+
+            when (radioGroup.checkedRadioButtonId) {
+                R.id.rbCar -> costPerHour = 2000
+                R.id.rbMinivan -> costPerHour = 2500
+                R.id.rbBus -> costPerHour = 3500
+            }
+
+            val totalCost = hours * costPerHour
+
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra("totalCost", totalCost)
+            startActivity(intent)
         }
     }
 }
